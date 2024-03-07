7941384efb9caa38aa948d546963801d273c0aa6
Viktor Nagy <viktor.nagy@365.bank>
2024-03-07 19:55:34 +0100

    feat: aceternity logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { AceternityLogo } from "./_components/aceternity-logo";
 import ParticleLogo from "./_components/particle-logo";
+import VocabularyLogo from "./_components/vocabulary-logo";
 
 export default async function Home() {
   return (
-    <ParticleLogo />
+    <>
+      <AceternityLogo title={"Portfolio"} />
+      <ParticleLogo />
+      <VocabularyLogo />
+    </>
   );
 }
