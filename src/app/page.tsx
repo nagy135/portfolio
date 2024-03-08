e826390c4fe9aa99bce29bd2ace9c59e71861999
Viktor Nagy <viktor.nagy@365.bank>
2024-03-08 09:53:27 +0100

    feat: descriptions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,17 @@ export default async function Home() {
   return (
     <>
       <AceternityLogo title={"Portfolio"} />
+      <p className="text-center m-3 mt-5">
+        Simple particles returning to original position (mouse move)
+      </p>
       <ParticleLogo />
+      <p className="text-center m-3">
+        Moving parts of svg using css (mouse move)
+      </p>
       <VocabularyLogo />
+      <p className="text-center m-3">
+        Generating svg on fly to shift lines (click)
+      </p>
       <ShiftingLines />
     </>
   );
